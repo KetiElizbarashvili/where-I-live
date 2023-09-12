82614214fcc72b3f8f3bfacd95105bd3b3d8ca72
KetiElizbarashvili <elizbarashviliketi44@gmail.com>
2023-09-12 12:00:00 +0000

    13

--- script.js
+++ script.js
@@ -7,10 +7,3 @@ rm fodelPosition = modelMatrix * vec4(position, 1.0);
 
 
 
-
-
-n() {
-        vUv = uv;
-        vec2 center = vec2(0.0, 0.0);
-        float distance = length(uv - center);
-     
