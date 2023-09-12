519ac94003e8eec7c51378178a62ba95423920ea
KetiElizbarashvili <elizbarashviliketi44@gmail.com>
2023-09-12 12:00:00 +0000

    float distance

---
+++ script.js
@@ -0,0 +1,19 @@
+n() {
+    vUv = uv;
+    vec2 center = vec2(0.0, 0.0);
+    float distance = length(uv - center);
+ 
+rm fodelPosition = modelMatrix * vec4(position, 1.0);
+
+
+
+
+
+n() {
+        vUv = uv;
+        vec2 center = vec2(0.0, 0.0);
+        float distance = length(uv - center);
+     
+rm fodelPosition = modelMatrix * vec4(position, 1.0);
+      
+ 
